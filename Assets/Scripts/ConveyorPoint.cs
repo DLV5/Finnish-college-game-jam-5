9f221667fcdcbdf90eccd46819044bd622ec50ed
Kameji <diman567tak@gmail.com>
2023-10-09 15:47:27 +0300

    Player can move points and create points from points

--- Assets/Scripts/ConveyorPoint.cs
+++ Assets/Scripts/ConveyorPoint.cs
@@ -1,18 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ConveyorPoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private ConveyorPoint _nextPoint;
+    private void OnMouseDrag()
     {
-        
+        //transform.position = GetMouseWorldPosition();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseUp()
     {
-        
+        CreateNextPoint();
+    }
+
+    private void CreateNextPoint()
+    {
+        _nextPoint = Instantiate(gameObject, GetMouseWorldPosition(), Quaternion.identity).GetComponent<ConveyorPoint>();
+        _nextPoint.gameObject.transform.position = GetMouseWorldPosition();
+    }
+
+    private Vector3 GetMouseWorldPosition()
+    {
+        Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        position.z = 0;
+
+        return position;
     }
 }
--- Assets/Scripts/Floor.cs
+++ Assets/Scripts/Floor.cs
@@ -3,8 +3,28 @@ using UnityEngine;
 public class Floor : MonoBehaviour
 {
     [SerializeField] private GameObject _conveyorPointPrefab;
+    [SerializeField] private LayerMask layerMask;
+
+    [SerializeField] private float _overlapCircleRadius;
     private void OnMouseDown()
     {
-        Instantiate( _conveyorPointPrefab, Input.mousePosition, Quaternion.identity);
+        Vector3 position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        position.z = 0;
+        if (!CheckAround(position))
+        {
+            Instantiate( _conveyorPointPrefab, position, Quaternion.identity);
+        }
+    }
+
+    private bool CheckAround(Vector3 position)
+    {
+        return Physics2D.OverlapCircle(position, _overlapCircleRadius, layerMask) != null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(Camera.main.ScreenToWorldPoint(Input.mousePosition), _overlapCircleRadius);
     }
 }
